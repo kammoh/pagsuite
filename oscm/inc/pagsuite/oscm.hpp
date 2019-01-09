895b3033496d577e40a99067ccebe6195a838826
kumm <kumm@a635d537-86be-40ee-9a26-da7f1c90a0bf>
2019-01-09 16:59:02 +0000

    reorganization of oscm for FloPoCo

---
+++ oscm/inc/pagsuite/oscm.hpp
@@ -0,0 +1,36 @@
+#ifndef SCMOPT_H
+#define SCMOPT_H
+
+#include <sstream>
+
+using namespace std;
+
+namespace PAGSuite
+{
+
+  class OptimalSCM
+  {
+  public:
+    OptimalSCM(int coeff, bool useOldAdderGraph = false);
+
+    string getAdderGraph();
+
+    int getAdderCost();
+
+  private:
+    int coeff;  /**< The constant */
+    int wIn;
+
+    void generateAOp(int a, int b, int c, int eA, int eB, int signA, int signB, int preFactor = 1);
+
+    void buildAdderGraph(int c, int preFactor = 1);
+
+    bool useOldAdderGraph;
+    stringstream adderGraph;
+
+    int adder_cost;
+  };
+
+}
+
+#endif // SCMOPT_H
---
+++ oscm/inc/pagsuite/scm_solutions.hpp
@@ -0,0 +1,31 @@
+#ifndef SCM_SOLUTIONS_HPP
+#define SCM_SOLUTIONS_HPP
+
+namespace PAGSuite
+{
+#define MAX_SCM_CONST 524287
+//#define MAX_SCM_CONST 317361 //limitation in Hcub solutions file
+
+  typedef enum
+  {
+    ADD, MUL, LF2, LF3
+  } graph_type_t;
+
+/*
+struct scm_solution_t
+{
+  int adders;
+  graph_type_t gt;
+  int op_a;
+  int op_b;
+};
+
+extern scm_solution_t solutions[];
+*/
+  extern int solutions_adders[];
+  extern graph_type_t solutions_gt[];
+  extern int solutions_op_a[];
+  extern int solutions_op_b[];
+
+}
+#endif // SCM_SOLUTIONS_HPP
